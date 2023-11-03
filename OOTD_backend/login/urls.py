7212a53d4a0b6de247c8f969192cc97d5fde7f81
duwanqing <duwanqingdwq@163.com>
2023-11-03 14:53:05 +0800

    忘提交了、、

--- OOTD_backend/login/urls.py
+++ OOTD_backend/login/urls.py
@@ -2,6 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('api/user/login/',views.login,name='login'),
-    path('api/user/logout/', views.logout, name='logout'),
+    path('api/user/login',views.login,name='login'),
+    path('api/user/logout', views.logout, name='logout'),
 ]
