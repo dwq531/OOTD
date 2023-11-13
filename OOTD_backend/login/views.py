342fd758c8c4453885e214782373987e430aaf5e
Rena <lihaoxua21@mails.tsinghua.edu.cn>
2023-11-13 21:29:31 +0800

    feat: 用户相关接口初步实现 还未实现参数验证 还未实现天气获取

--- OOTD_backend/login/views.py
+++ OOTD_backend/login/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse, JsonResponse
 from utils.jwt import encrypt_password, generate_jwt, login_required
 import json
 from login import controllers
+from django.utils import timezone
+
 
 
 def login(request):
@@ -33,18 +35,91 @@ def login(request):
                 'avatarUrl': user.avatarUrl,    
                 'updated': user.updated,      
                 'message': 'ok'
-            }
-            )
+            }, status = 200)
         else:
             return JsonResponse({"message": "Invalid credentials"}, status=401)
     except:
         return JsonResponse({"message": "bad arguments"}, status=400)
 
 
-
+@login_required
 def logout(request):
-    return HttpResponse('退出')
+    return JsonResponse({"message": "ok"}, status=200)
 
 
-def index(request):
-    return HttpResponse('主页面')
+@login_required
+def user(request):
+    """
+    获取用户信息
+    """
+    if request.method != "GET":
+        return JsonResponse({"message": "Method not allowed"}, status=405)
+    
+    try:
+        user = request.user
+        
+        return JsonResponse({
+            'nickname': user.nickname,
+            'age': user.age,
+            'addr': user.addr,
+            'gender': user.gender,
+            'phone': user.phone,
+            'intro': user.intro,
+            'avatarUrl': user.avatarUrl,
+            'updated': user.updated,
+            'message': 'ok'
+        }, status=200)
+    except:
+        return JsonResponse({"message": "Internal Server Error"}, status=500)
+
+
+@login_required
+def addr(request):
+    return JsonResponse({"message": "Not Implemented"}, status=501)
+
+
+@login_required
+def edit_info(request):
+    """
+    修改用户信息
+    """
+    if request.method != "PATCH":
+        return JsonResponse({"message": "Method not allowed"}, status=405)
+        
+    try:
+        user = request.user
+        
+        #TODO: 检查参数正确性
+        if False:
+            return JsonResponse({"message": "Invalid argument"}, status=400)
+        
+        
+        if (request.avatarUrl):
+            user.avatarUrl = request.avatarUrl
+        if (request.nickname):
+            user.nickname = request.nickname
+        if (request.gender):
+            user.gender = request.gender
+        if (request.phone):
+            user.phone = request.phone
+        if (request.intro):
+            user.intro = request.intro
+        
+        user.updated = timezone.now()
+        
+        return JsonResponse({"updated": user.updated, "message": "ok"}, status=200)
+        
+    except:
+        return JsonResponse({"message": "Internal Server Error"}, status=500)
+
+'''
+openid 感觉没必要返回给用户，jwt 已经完成了
+（话说用这么多助教的代码没问题吗...）
+'''
+
+
+
+# 感觉好像没用，先注释了
+#
+# def index(request):
+#     return HttpResponse('主页面')
