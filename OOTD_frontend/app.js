32a11444e61197f33de5b7b14b3d90484aaf563a
gff <gff21@mails.tsinghua.edu.cn>
2023-11-24 14:50:23 +0800

    fix:登录和后端连接成功，不报错了

--- OOTD_frontend/app.js
+++ OOTD_frontend/app.js
@@ -5,13 +5,14 @@ App({
     const logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
+    const that = this;
 
     // 登录
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
         wx.request({
-          url: 'http://127.0.0.1:8000/api/user/login/',
+          url: 'http://127.0.0.1:8000/api/user/login',
           method: 'PATCH',
           data: {
             code : res.code
@@ -23,25 +24,25 @@ App({
               // TODO
               // 处理后端返回的数据
               const isNewUser = res.data.new;
-              this.globalData.isNewUser = isNewUser;
+              that.globalData.isNewUser = isNewUser;
               const jwt = res.data.jwt;
-              this.globalData.jwt = jwt;
+              that.globalData.jwt = jwt;
               const nickname = res.data.nickname;
-              this.globalData.nickname = nickname;
+              that.globalData.nickname = nickname;
               const age = res.data.age;
-              this.globalData.age = age;
+              that.globalData.age = age;
               const addr = res.data.addr;
-              this.globalData.addr = adde;
+              that.globalData.addr = addr;
               const gender = res.data.gender;
-              this.globalData.gender = gender;
+              that.globalData.gender = gender;
               const phone = res.data.phone;
-              this.globalData.phone = phone;
+              that.globalData.phone = phone;
               const intro = res.data.intro;
-              this.globalData.intro = intro;
+              that.globalData.intro = intro;
               const avatarUrl = res.data.avatarUrl;
-              this.globalData.avatarUrl = avatarUrl;
+              that.globalData.avatarUrl = avatarUrl;
               const updated = res.data.updated;
-              this.globalData.updated = updated;
+              that.globalData.updated = updated;
             }
             else{
               console.error('请求失败，错误状态码：', res.statusCode);
