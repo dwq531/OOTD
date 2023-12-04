85a3fee553e2b6644052eeebe45fbdab8b95011c
cz <1980103789@qq.com>
2023-12-04 20:32:48 +0800

    feat: 简单连接部分前后端

--- OOTD_frontend/pages/personalCenter/personalCenter.js
+++ OOTD_frontend/pages/personalCenter/personalCenter.js
@@ -1,4 +1,5 @@
 // personalCenter.js
+const app = getApp()
 
 Page({
   
@@ -6,10 +7,16 @@ Page({
   onLoad: function () {
     // 页面加载时的初始化操作，可以在这里处理数据加载等任务
     //console.log("页面加载完成");
+    console.log("nickname:",app.globalData.nickname);
   },
 
   //下拉框选择月份
   data: {
+    avatarUrl:app.globalData.avatarUrl,
+    nickname:app.globalData.nickname,
+    gender:app.globalData.gender,
+    age:app.globalData.age,
+    addr:app.globalData.addr,
     months: ['1月', '2月', '3月', '4月', '5月', '6月'],
     selectedMonth: '月份',
   },
