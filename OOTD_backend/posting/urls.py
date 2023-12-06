518d63adb53d540d82db77363289152f5b27eb56
Rena <lihaoxua21@mails.tsinghua.edu.cn>
2023-12-06 17:09:34 +0800

    init: 初步实现 posting 相关后端
    需要修改视图的返回格式

---
+++ OOTD_backend/posting/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import create_post, post_detail, user_posts
+
+urlpatterns = [
+    # 贴文创建
+    path('create_post/', create_post, name='create_post'),
+
+    # 贴文详情
+    path('post_detail/id=<int:post_id>/', post_detail, name='post_detail'),
+
+    # 用户贴文列表
+    path('user_posts/<str:post_type>/', user_posts, name='user_posts'),
+]
