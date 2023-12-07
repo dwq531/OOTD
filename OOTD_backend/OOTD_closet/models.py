328de7fafbfc1da48478d5ed61769af1e82fb575
gff <gff21@mails.tsinghua.edu.cn>
2023-12-07 11:08:36 +0800

    feat:添加衣服

--- OOTD_backend/OOTD_closet/models.py
+++ OOTD_backend/OOTD_closet/models.py
@@ -1,17 +1,35 @@
 from django.db import models
+from django.core.files.base import ContentFile
+import requests
 
 # Create your models here.
+
+class Type(models.TextChoices):
+    UPPER = 'upper','上装'
+    BOTTOM = 'bottom','下装'
+    SHOES = 'shoes','鞋'
+    BAG = 'bag','包'
+    ACCESSORIES = 'accessories','首饰'
+
 class Clothes(models.Model):
     """
     衣服
     """
+    clothesid = 0
+    clothes_ID = models.IntegerField(default=0,verbose_name="衣服id")
     clothes_name = models.CharField(max_lenghth=32,default='',verbose_name="衣服名字")
-    clothes_main_type = models.CharField(max_lenghth=32,default='',verbose_name="衣服主要类型")
-    coat_type = models.CharField(max_lenghth=32,default='',verbose_name="外套类型")
-    dress_type = models.CharField(max_lenghth=32,default='',verbose_name="裙子类型")
-    upper_type = models.CharField(max_lenghth=32,default='',verbose_name="上衣类型")
-    pants_type = models.CharField(max_lenghth=32,default='',verbose_name="裤装类型")
-    skirt_type = models.CharField(max_lenghth=32,default='',verbose_name="半身裙类型")
-    shoes_type = models.CharField(max_lenghth=32,default='',verbose_name="鞋子类型")
-    bag_type = models.CharField(max_lenghth=32,default='',verbose_name="包包类型")
-    accessories_type = models.CharField(max_lenghth=32,default='',verbose_name="配饰类型")
+    clothes_main_type = models.CharField(max_length=32,choices=Type.choices,default=Type.UPPER,verbose_name="衣服主要类型")
+    clothes_detail_type = models.CharField(max_lenghth=32,default='',verbose_name="衣服细分类型")
+    clothes_picture_url=models.CharField(max_length=255,default='/',verbose_name="衣服图片url")   # 在服务器上的 url
+    clothes_picture = models.ImageField(upload_to='images/')   # 指定衣服储存的目录
+    clothes_used_time = models.IntegerField(default=0,verbose_name="衣服使用次数")
+    
+    def save_image_from_url(self):
+        response = requests.get(self.clothes_picture_url)
+        if response.status_code == 200:
+            self.clothes_picture_url = 'clothes/' + f'{self.clothesid}_clothes.jpg'
+            self.clothes_picture.save(self.clothes_picture_url, ContentFile(response.content))
+        else:
+            # TODO 将 None 替换为默认头像
+            self.clothes_picture_url = None
+            self.clothes_picture = None
--- OOTD_backend/OOTD_closet/views.py
+++ OOTD_backend/OOTD_closet/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+from .models import Clothes
 
-# Create your views here.
+@require_POST
+def add_clothes(request):
+    """
+        添加衣服
+    """
+    if request.method != "POST":
+        return JsonResponse({"message": "Method not allowed"}, status=405)
+    try:
+        content = request.POST
+        clothes_ID = Clothes.clothesid + 1
+        clothes_name = content.get("name")
+        clothes_main_type = content.get("Mtype")
+        clothes_detail_type = content.get("Dtype")
+        clothes_picture_url = content.get("pictureUrl")
+
+        new_clothes = Clothes(
+            clothes_ID = clothes_ID,
+            clothes_name = clothes_name,
+            clothes_main_type = clothes_main_type,
+            clothes_detail_type = clothes_detail_type,
+            clothes_picture_url = clothes_picture_url,
+        )
+        new_clothes.save_image_from_url()
+        new_clothes.save()
+
+        return JsonResponse({"message": "Clothes added to the wardrobe successfully"}, status=201)
+
+    except Exception as e:
+        print(e)
+        return JsonResponse({"message": "Internal Server Error"}, status=500)
+
