f718c1b0e10d7627f4597d6a49dee2358cbb34d4
gff <gff21@mails.tsinghua.edu.cn>
2023-12-08 15:30:26 +0800

    fix:添加到urls和apps

--- OOTD_backend/OOTD_closet/urls.py
+++ OOTD_backend/OOTD_closet/urls.py
@@ -2,6 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('api/user/add_clothes',views.add_clothes,name='add_clothes'),
-    path('api/user/edit_clothes', views.edit_clothes, name='edit_clothes'),
+    path('api/closet/add_clothes',views.add_clothes,name='add_clothes'),
+    path('api/closet/edit_clothes', views.edit_clothes, name='edit_clothes'),
 ]
