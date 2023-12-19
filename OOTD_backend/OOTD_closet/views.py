5c361f0f89d441d1285ca5f15940e672d0c47f48
gff <gff21@mails.tsinghua.edu.cn>
2023-12-19 16:19:24 +0800

    fix:用表单实现修改衣服和删除衣服

--- OOTD_backend/OOTD_closet/views.py
+++ OOTD_backend/OOTD_closet/views.py
@@ -8,11 +8,22 @@ from .models import Type
 import datetime
 from django import forms
 from django.shortcuts import get_object_or_404
+from django.http import HttpResponseNotAllowed
 
-class ClothesForm(forms.ModelForm):
+class AddClothesForm(forms.ModelForm):
     class Meta:
         model = Clothes
         fields = ['clothes_name', 'clothes_main_type', 'clothes_detail_type']
+        
+class DeleteClothesForm(forms.ModelForm):
+    class Meta:
+        model = Clothes
+        fields = ['clothes_ID']
+        
+class EditClothesForm(forms.ModelForm):
+    class Meta:
+        model = Clothes
+        fields = ['clothes_ID','clothes_name', 'clothes_main_type', 'clothes_detail_type']
 
 @login_required
 def add_clothes(request):
@@ -20,7 +31,7 @@ def add_clothes(request):
     添加衣服
     """
     if request.method == "POST":
-        form = ClothesForm(request.POST)
+        form = AddClothesForm(request.POST)
         if not form.is_valid():
             print("Invalid arguments")
             return JsonResponse({"message": "Invalid arguments"}, status=402)
@@ -37,123 +48,62 @@ def add_clothes(request):
         return JsonResponse({"clothesid":new_clothes.clothesid, "message": "Clothes added to the closet successfully"}, status=200)
     
     elif request.method == "GET":
-        form = ClothesForm()
+        form = AddClothesForm()
         return JsonResponse({"message": "empty"}, status=200)
     
     else:
         return JsonResponse({"message": "Method not allowed"}, status=405)
 
-"""
 @login_required
-def add_clothes(request):
-    if request.method != "POST":
-        return JsonResponse({"message": "Method not allowed"}, status=405)
-    try:
-        # print(request.body)
-        content = json.loads(request.body)
+def delete_clothes(request):
+    """
+    删除衣服
+    """
+    if request.method == "POST":
+        form = DeleteClothesForm(request.POST)
+        if not form.is_valid():
+            print("Invalid arguments")
+            return JsonResponse({"message": "Invalid arguments"}, status=402)
         
-        clothes_ID = Clothes.clothesid + 1
-        Clothes.clothesid += 1
-        clothes_name = content.get("name")
-        clothes_main_type = content.get("Mtype")
-        clothes_detail_type = content.get("Dtype")
-        # clothes_picture_url = content.get("pictureUrl")
+        clothes = get_object_or_404(Clothes, id=form.cleaned_data['clothes_ID'])
+        clothes.delete()    
+        return JsonResponse({"message": "Clothes deleted successfully"}, status=200)
+    
+    elif request.method == "GET":
+        form = DeleteClothesForm()
+        return JsonResponse({"message": "empty"}, status=200)
+    
+    else:
+        return JsonResponse({"message": "Method not allowed"}, status=405)
 
-        new_clothes = Clothes(
-            clothes_ID = clothes_ID,
-            clothes_name = clothes_name,
-            clothes_main_type = clothes_main_type,
-            clothes_detail_type = clothes_detail_type,
-            # clothes_picture_url = clothes_picture_url,
-        )
-        # new_clothes.save_image_from_url()
+@login_required
+def edit_clothes(request, clothes_ID):
+    """
+    编辑衣服
+    """
+    if request.method == "POST":
+        form = EditClothesForm(request.POST)
+        if not form.is_valid():
+            print("Invalid arguments")
+            return JsonResponse({"message": "Invalid arguments"}, status=402)
+        
+        clothes = get_object_or_404(Clothes, id=form.cleaned_data['clothes_ID'])
+        clothes.clothes_name = form.cleaned_data['clothes_name']
+        clothes.clothes_main_type = form.cleaned_data['clothes_main_type']
+        clothes.clothes_detail_type = form.cleaned_data['clothes_detail_type']
         uploaded_file = request.FILES['file'] 
-        user = request.user
-        new_clothes.user = user
-        # print(Clothes.clothes_picture_url)
-        new_clothes.clothes_picture_url = 'clothes/' + f'{new_clothes.clothesid}_clothes.jpg'
-        new_clothes.clothes_picture.save(new_clothes.clothes_picture_url, uploaded_file)
-        new_clothes.save()
-        new_clothes.updated = timezone.now()
-
-        return JsonResponse({"message": "Clothes added to the closet successfully"}, status=201)
-
-    except Exception as e:
-        print(e)
-        return JsonResponse({"message": "Internal Server Error"}, status=500)
-"""
-
-@login_required
-def edit_clothes(request):
-    """
-    修改衣服信息
-    """
-    if request.method != "PATCH":
-        return JsonResponse({"message": "Method not allowed"}, status=405)
-        
-    try:
-        content = json.loads(request.body)
-        clothes_id = content.get('clothes_ID', None)
-        clothes_id = int(clothes_id)
-        # 在数据库中查找相应的 Clothes 对象
-        clothes = Clothes.objects.get(clothes_ID=clothes_id)
-        if (content.get('pictureUrl')):
-            clothes.clothes_picture_url = content['pictureUrl']
-            clothes.save_image_from_url()
-            if clothes.clothes_picture is None:
-                return JsonResponse({"message": "Invalid avatarUrl"}, status=400)
-        
-        if (clothes.get('name')):
-            if len(content['name'])>32:
-                return JsonResponse({"message": "Invalid nickname"}, status=400)
-            else:
-                clothes.name = content['name']
-
-        if (content.get('Mtype')):
-            if content['Mtype']=='上装':
-                clothes.clothes_main_type = Type.UPPER
-            elif content['Mtype']=='下装':
-                clothes.clothes_main_type = Type.BOTTOM
-            elif content['Mtype']=='鞋':
-                clothes.clothes_main_type = Type.SHOES
-            elif content['Mtype']=='包':
-                clothes.clothes_main_type = Type.BAG
-            elif content['Mtype']=='首饰':
-                clothes.clothes_main_type = Type.ACCESSORIES
-            else:
-                return JsonResponse({"message": "Invalid gender"}, status=400)
-        
-        if (clothes.get('Dtype')):
-            if len(content['Dtype'])>32:
-                return JsonResponse({"message": "Invalid Dtype"}, status=400)
-            else:
-                clothes.clothes_detail_type = content['Dtype']
-
+        clothes.clothes_picture.delete(save=False)
+        clothes.clothes_picture_url = 'clothes/' + f'{clothes.clothesid}_clothes.jpg'
+        clothes.clothes_picture.save(clothes.clothes_picture_url, uploaded_file)
         clothes.save()
-        clothes.updated = timezone.now()
-        
-        return JsonResponse({"clothes_ID":clothes.clothes_ID, "pictureUrl":clothes.clothes_picture_url, "update":clothes.updated, "message": "ok"}, status=200)
-        
-    except Exception as e:
-        print(e)
-        return JsonResponse({"message": "Internal Server Error"}, status=500)
-
-@login_required
-def upload_file(request):
-    try:
-        if request.method == 'POST':
-            uploaded_file = request.FILES['file'] 
-            Clothes = request.user
-            print(Clothes.clothes_picture_url)
-            #删除原来的图片
-            Clothes.clothes_picture.delete(save=False)
-            Clothes.clothes_picture_url = 'avatars/' + f'{Clothes.clothesid}_avatar.jpg'
-            Clothes.clothes_picture.save(Clothes.clothes_picture_url, uploaded_file)
-
-            return JsonResponse({"avatarUrl":Clothes.clothes_picture_url, "updated": Clothes.updated,"message": "ok"}, status=200)
-    except Exception as e:
-        print(e)
-        return JsonResponse({"message": "Internal Server Error"}, status=500)
+        return JsonResponse({"message": "Clothes edited successfully"}, status=200)
+    
+    elif request.method == "GET":
+        form = EditClothesForm()
+        return JsonResponse({"message": "empty"}, status=200)
+    
+    else:
+        return JsonResponse({"message": "Method not allowed"}, status=405)
 
 # 获取衣服列表
 @login_required
