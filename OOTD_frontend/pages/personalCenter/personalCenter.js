cca82b621287b461fbc3745d476f9419c0099d6c
cz <1980103789@qq.com>
2023-12-22 15:43:55 +0800

    feat: 评分折线统计图前端写完

--- OOTD_frontend/pages/personalCenter/personalCenter.js
+++ OOTD_frontend/pages/personalCenter/personalCenter.js
@@ -13,8 +13,10 @@ Page({
     following:"",
     posts:"",
     followers:"",
-    months: ['1月', '2月', '3月', '4月', '5月', '6月'],
-    selectedMonth: '月份',
+    period: ['最近一周', '最近一月', '最近一年'],
+    selectedMonth: '时间选择',
+    canvasWidth: 320,
+    canvasHeight: 200,
   },
 
   // 页面加载时执行的函数
@@ -54,7 +56,8 @@ Page({
         console.error('Failed to request user info:', err);
       }
     });
-    console.log("avatarUrl:",this.data.avatarUrl);
+    // console.log("avatarUrl:",this.data.avatarUrl);
+    this.getWeeklyRatings();
   },
   onHide: function() {
     // 在页面离开时清理数据
@@ -109,6 +112,7 @@ Page({
         console.error('Failed to request user info:', err);
       },
     });
+    this.getWeeklyRatings();
   },
   
   onPickerChange: function (e) {
@@ -122,6 +126,78 @@ Page({
     wx.navigateTo({
       url: '/pages/informationEditor/informationEditor',
     })
-  }
+  },
 
+  getWeeklyRatings: function(){
+    wx.request({
+      url: 'http://127.0.0.1:8000/api/user/ratings', // 替换为实际的API地址和用户ID
+      method: 'GET',
+      header: {
+        'Authorization': app.globalData.jwt, // 添加 JWT Token
+      },
+      success: (res) => {
+        if (res.statusCode === 200){
+          // 处理数据，将日期和评分分别提取出来
+          const dates = res.data.dates;
+          const scores = res.data.ratings;
+
+          // 绘制折线图
+          this.drawLineChart(dates, scores);
+        }
+      },
+      fail: (error) => {
+        console.error('Failed to fetch ratings data:', error);
+      }
+    });
+  },
+
+  // 绘制折线图
+  drawLineChart: function (dates, scores) {
+    wx.createSelectorQuery()
+    .select('#lineCanvas') // 在 WXML 中填入的 id
+    .node(({ node: canvas }) => {
+        const ctx = canvas.getContext('2d');
+        
+        // 设置坐标系原点
+        const originX = 30;
+        const originY = 180;
+
+        // 计算横坐标和纵坐标的间隔
+        const xGap = (this.data.canvasWidth - originX * 2) / (dates.length - 1);
+        const yGap = (originY - 20) / (Math.max(...scores) - 0);
+
+        // 绘制横坐标轴
+        ctx.moveTo(originX, originY);
+        ctx.lineTo(this.data.canvasWidth - originX, originY);
+        ctx.stroke();
+
+        // 绘制纵坐标轴
+        ctx.moveTo(originX, originY);
+        ctx.lineTo(originX, 20);
+        ctx.stroke();
+
+        // 绘制折线
+        ctx.beginPath();
+        ctx.moveTo(originX, originY - (scores[0] - 0) * yGap);
+        for (let i = 1; i < dates.length; i++) {
+          ctx.lineTo(originX + i * xGap, originY - (scores[i] - 0) * yGap);
+        }
+        ctx.stroke();
+        ctx.closePath();
+
+        // 绘制横坐标刻度
+        for (let i = 0; i < dates.length; i++) {
+          ctx.fillText(dates[i], originX + i * xGap - 10, originY + 20);
+        }
+
+        // 绘制纵坐标刻度
+        for (let i = 0; i <= Math.max(...scores); i++) {
+          ctx.fillText(i, originX - 25, originY - i * yGap + 5);
+        }
+
+        // 执行绘图操作
+        ctx.draw();
+    })
+    .exec()
+  },
 });
