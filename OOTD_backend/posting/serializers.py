da51e73dd5e86d0f3fe4d2522a8fc2ff41ac4a03
Rena <lihaoxua21@mails.tsinghua.edu.cn>
2023-12-25 16:59:45 +0800

    fix: 修复 comment 响应不包含用户详细信息的问题

--- OOTD_backend/posting/serializers.py
+++ OOTD_backend/posting/serializers.py
@@ -10,15 +10,17 @@ class ImageSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
-class CommentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Comment
-        fields = "__all__"
-
-
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
+        fields = "__all__"
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Comment
         fields = "__all__"
 
 
