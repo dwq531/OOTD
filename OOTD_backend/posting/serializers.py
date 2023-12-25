e0993fc743717f1271c8b31e477be09ec27b2bd9
Rena <lihaoxua21@mails.tsinghua.edu.cn>
2023-12-25 16:54:54 +0800

    feat: 修改序列化器，现在可以返回用户的详细信息

--- OOTD_backend/posting/serializers.py
+++ OOTD_backend/posting/serializers.py
@@ -1,6 +1,7 @@
 # posts/serializers.py
 from rest_framework import serializers
 from .models import Post, Image, Comment
+from login.models import User
 
 
 class ImageSerializer(serializers.ModelSerializer):
@@ -15,7 +16,14 @@ class CommentSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = "__all__"
+
+
 class PostSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
     images = ImageSerializer(many=True, read_only=True)
     comments = CommentSerializer(many=True, read_only=True)
 
