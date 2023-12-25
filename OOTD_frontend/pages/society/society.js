3507788e35104eced62d892ae39fc5ca59958903
dwq531 <duwanqingdwq@163.com>
2023-12-25 21:49:43 +0800

    feat:浏览帖子前端完成

--- OOTD_frontend/pages/society/society.js
+++ OOTD_frontend/pages/society/society.js
@@ -1,7 +1,26 @@
 const app = getApp()
 Page({
   data:{
-    post:[]
+    post:[],
+    left:[],
+    right:[],
+    menu_item:["全部","我的","收藏"],
+    curIndex:0
+  },
+  // 把post列表拆分成两列瀑布流
+  splitPost:function(post){
+    var left=[],right=[]
+    for(let i=0;i<post.length;i++)
+    {
+      if(i%2===0)
+        left.push(post[i])
+      else
+        right.push(post[i])
+    }
+    this.setData({
+      left:left,
+      right:right
+    })
   },
   onShow: function(e) {
     const that = this
@@ -14,7 +33,7 @@ Page({
         'Authorization': app.globalData.jwt
       },
       success: function(res) {
-        console.log(res.data.posts); 
+        that.splitPost(res.data.posts)
         that.setData({
           post:res.data.posts
         })
@@ -26,5 +45,28 @@ Page({
     wx.navigateTo({
       url:"/pages/postDetail/postDetail?"+"&index="+index,
     })
+  },
+  changeMenu:function(e){
+    const index = parseInt(e.currentTarget.dataset.index)
+    const url_name = ['all','created','favorite']
+    const that = this
+    wx.request({
+      url: 'http://127.0.0.1:8000/api/posting/user_posts/'+url_name[index], 
+      method: 'GET',
+      header: {
+        'content-type': 'application/json',
+        'Authorization': app.globalData.jwt
+      },
+      success: function(res) {
+        console.log(res.data.posts)
+        that.splitPost(res.data.posts) 
+        that.setData({
+          post:res.data.posts
+        })
+      }
+    });
+    this.setData({
+      curIndex: index
+  })
   }
 })
